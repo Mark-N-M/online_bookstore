fc1191e79dbb081be483dce493b0a480abc83d02
Mark-N-M <marknm5757@gmail.com>
2026-06-22 09:04:41 +0300

    comment changes

--- src/stores/wishlist.js
+++ src/stores/wishlist.js
@@ -1,15 +1,19 @@
 import { defineStore } from 'pinia'
 
+//<this a state being defined allowing and individual to source data from this specific store all over all of the components
 export const useWishlistStore = defineStore('wishlist', {
   state: () => {
     const wishlist = []
     return { wishlist }
   },
+//<this a state being defined allowing and individual to source data from this specific store all over all of the components
+
+
   actions: {
     addToWishlist(payload) {
-      const alreadyAdded = this.wishlist.some(item => item.id === payload.id)
+      const alreadyAdded = this.wishlist.some(item => item.id === payload.id) 
       if (!alreadyAdded) {
-        this.wishlist.push(payload)
+        this.wishlist.push(payload)  //payload refers to the item that we want to add to wishlist in our case books from the Books.vue component
       }
     },
     removeFromWishlist(id) {
