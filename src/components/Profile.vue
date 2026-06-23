28ddf6af4fb8dc8dc99370de4e9871899c9b4bc2
Mark-N-M <marknm5757@gmail.com>
2026-06-23 21:03:07 +0300

    personal testing

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,5 +3,13 @@ const user =JSON.parse(localStorage.getItem('user'))
 </script>
 
 <template>
-    <h1>{{ user.lastname }}</h1>
+    <v-container>
+        <v-card>
+            <v-avatar icon="$vuetify" image="loginMsymbol.jpg"></v-avatar>
+            <h1 align="center" mr="10">About Me</h1>
+            <p>Manage your personal info and control how you connect with the community across our bookstore services. Update your reading preferences, track your orders, and choose what others see on your public profile.</p>
+            
+        </v-card>
+
+    </v-container>
 </template> 
