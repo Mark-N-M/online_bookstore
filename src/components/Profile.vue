c470544c2e07716e6ba8793165cb201299a96d08
Mark-N-M <marknm5757@gmail.com>
2026-06-23 21:18:42 +0300

    replacement.png and profile adjustments

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,13 +3,93 @@ const user =JSON.parse(localStorage.getItem('user'))
 </script>
 
 <template>
-    <v-container>
-        <v-card>
-            <v-avatar icon="$vuetify" image="loginMsymbol.jpg"></v-avatar>
-            <h1 align="center" mr="10">About Me</h1>
-            <p>Manage your personal info and control how you connect with the community across our bookstore services. Update your reading preferences, track your orders, and choose what others see on your public profile.</p>
-            
-        </v-card>
+      <v-container v-if="user" class="mt-8">
+    <v-row>
+      <v-col cols="auto">
+        <v-btn icon="mdi-arrow-left" variant="text" @click="router.back()"></v-btn>
+      </v-col>
+      <v-col>
+        <div class="text-h5 mt-2">About me</div>
+      </v-col>
+    </v-row>
 
-    </v-container>
-</template> 
+    <v-row class="mt-4">
+      <v-col cols="auto">
+        <!-- AVATAR IMAGE GOES HERE -->
+        <!-- swap '/replacement.jpg' for the exact filename + extension you upload -->
+        <v-avatar size="100" :image="'/replacement.jpg'"></v-avatar>
+      </v-col>
+      <v-col class="d-flex align-center">
+        <div class="text-h6">{{ user.firstname }} {{ user.lastname }}</div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-6">
+      <v-col cols="12">
+        <v-card class="pa-4">
+          <v-row align="center">
+            <v-col cols="1"><v-icon icon="mdi-account"></v-icon></v-col>
+            <v-col>
+              <div class="text-caption text-grey">Name</div>
+              <div>{{ user.firstname }} {{ user.lastname }}</div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12">
+        <v-card class="pa-4">
+          <v-row align="center">
+            <v-col cols="1"><v-icon icon="mdi-email"></v-icon></v-col>
+            <v-col>
+              <div class="text-caption text-grey">Email</div>
+              <div>{{ user.email }}</div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12">
+        <v-card class="pa-4">
+          <v-row align="center">
+            <v-col cols="1"><v-icon icon="mdi-phone"></v-icon></v-col>
+            <v-col>
+              <div class="text-caption text-grey">Phone</div>
+              <div>{{ user.phone }}</div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12">
+        <v-card class="pa-4">
+          <v-row align="center">
+            <v-col cols="1"><v-icon icon="mdi-map-marker"></v-icon></v-col>
+            <v-col>
+              <div class="text-caption text-grey">Location</div>
+              <div>{{ user.location }}</div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12">
+        <v-card class="pa-4">
+          <v-row align="center">
+            <v-col cols="1"><v-icon icon="mdi-home"></v-icon></v-col>
+            <v-col>
+              <div class="text-caption text-grey">Address</div>
+              <div>{{ user.address }}</div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container v-else class="mt-8 text-center">
+    <div class="text-h6">No profile found.</div>
+    <div>Please sign up first.</div>
+    <v-btn class="mt-4" color="primary" to="/sign-up">Go to Sign Up</v-btn>
+  </v-container>
+</template>
